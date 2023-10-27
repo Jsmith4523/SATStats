25c0666e24cf078c130084439d08a1bfb1dacf0f
Jaylen Smith <kobe26945@icloud.com>
2023-10-27 00:35:53 -0400

    Initial commit

---
+++ SATStats/Extensions/URLSession.swift
@@ -0,0 +1,8 @@
+//
+//  URLSession.swift
+//  SATStats
+//
+//  Created by Jaylen Smith on 10/26/23.
+//
+
+import Foundation
---
+++ SATStats/Model/School.swift
@@ -0,0 +1,8 @@
+//
+//  School.swift
+//  SATStats
+//
+//  Created by Jaylen Smith on 10/26/23.
+//
+
+import Foundation
